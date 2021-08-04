32ffe3a9e6d7792cc49a1ce4d99cf58d77cf1107
Firos vp <firosvp333@gmail.com>
2021-08-04 09:39:32 +0530

    first commit

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
